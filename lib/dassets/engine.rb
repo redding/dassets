6f6f9c62d6386d6d268a1b46757edb16c6293d61
Kelly Redding <kelly@kellyredding.com>
2013-04-23 05:50:45 -0500

    base Engine class and NullEngine implementation
    This is the base Engine for compiling and processing assets.  It
    defines the API for an engine and should be subclassed when creating
    new engines.
    
    This also includes the NullEngine implementation.  It simply returns
    exactly what it is given and can be thought of as a - it does nothing.
    This will be used as the default engine for compiling assets.

---
+++ lib/dassets/engine.rb
@@ -0,0 +1,33 @@
+module Dassets
+
+  class Engine
+
+    attr_reader :opts
+
+    def initialize(opts)
+      @opts = opts
+    end
+
+    def ext(input_ext)
+      raise NotImplementedError
+    end
+
+    def compile(input)
+      raise NotImplementedError
+    end
+
+  end
+
+  class NullEngine < Engine
+
+    def ext(input_ext)
+      input_ext
+    end
+
+    def compile(input)
+      input
+    end
+
+  end
+
+end
---
+++ test/unit/engine_tests.rb
@@ -0,0 +1,54 @@
+require 'assert'
+require 'dassets/engine'
+
+class Dassets::Engine
+
+  class BaseTests < Assert::Context
+    desc "the base Dassets::Engine"
+    setup do
+      @engine = Dassets::Engine.new('some' => 'opts')
+    end
+    subject{ @engine }
+
+    should have_reader :opts
+    should have_imeths :ext, :compile
+
+    should "know its opts" do
+      exp_opts = {'some' => 'opts'}
+      assert_equal exp_opts, subject.opts
+    end
+
+    should "raise NotImplementedError on `ext` and `compile`" do
+      assert_raises NotImplementedError do
+        subject.ext('foo')
+      end
+
+      assert_raises NotImplementedError do
+        subject.compile('some content')
+      end
+    end
+
+  end
+
+  class NullEngineTests < Assert::Context
+    desc "the NullEngine"
+    setup do
+      @engine = Dassets::NullEngine.new('some' => 'opts')
+    end
+    subject{ @engine }
+
+    should "be a Engine" do
+      assert_kind_of Dassets::Engine, subject
+    end
+
+    should "return the given extension on `ext`" do
+      assert_equal 'foo', subject.ext('foo')
+    end
+
+    should "return the given input on `compile" do
+      assert_equal 'some content', subject.compile('some content')
+    end
+
+  end
+
+end
