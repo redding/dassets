67daca427ab74a8a50fc9547ed79841ab05e28af
Kelly Redding <kelly@kellyredding.com>
2013-04-25 12:09:01 -0500

    redesign: tweak digest cmd implementation to always use active state
    Was running into problems b/c the modifications made by the digest
    cmd weren't on the active objects in use by Dassets.
    
    This just simplifies and cleans things up to always use the active
    (ie global) state when digesting.

--- test/unit/cmds/digest_cmd_tests.rb
+++ test/unit/cmds/digest_cmd_tests.rb
@@ -7,33 +7,15 @@ class Dassets::Cmds::DigestCmd
   class BaseTests < Assert::Context
     desc "Dassets::Cmds::DigestCmd"
     setup do
-      @cmd = Dassets::Cmds::DigestCmd.new(['a/path'], {'some' => 'opts'})
+      @cmd = Dassets::Cmds::DigestCmd.new(['a/path'])
     end
     subject{ @cmd }
 
-    should have_readers :source_paths, :opts
+    should have_readers :paths
     should have_instance_method :run
-    should have_class_method :for
 
-    should "know it's source files and opts" do
-      assert_equal(['a/path'], subject.source_paths)
-      assert_equal({'some' => 'opts'}, subject.opts)
-    end
-
-    should "clear the output path and digests if run for no specific paths" do
-      exp_opts = {
-        :clear_output_path => Dassets.config.output_path,
-        :clear_digests     => Dassets.digests
-      }
-      assert_equal exp_opts, Dassets::Cmds::DigestCmd.for([]).opts
-
-      exp_opts = {}
-      assert_equal exp_opts, Dassets::Cmds::DigestCmd.for(['a/path']).opts
-    end
-
-    should "run on all of the sources if run for no specific paths" do
-      exp_sources = Dassets::SourceList.new(Dassets.config)
-      assert_equal exp_sources, Dassets::Cmds::DigestCmd.for([]).source_paths
+    should "know it's paths" do
+      assert_equal ['a/path'], subject.paths
     end
 
   end
